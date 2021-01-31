65b358ec746e53c5cffe973d0390e4a3f2d0c8a9
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-01-31 15:17:30 +0200

    Add incapsulating functions

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -4,31 +4,36 @@ new Map()
   .set('www.facebook.com', 'src_to_icon')
   .set('www.facebook.com', 'src_to_icon');
 
-  const socialSites = new Map([[1, 'www.facebook.com'],[2, 'twitter.com'],[3, 'www.instagram.com']]);
-
   const container = document.getElementById('root');
   const cards = responseData.map((data) => createCard(data));
   container.append(...cards);
 
   function createCard(data) {
-    const {firstName, lastName, profilePicture, contacts: {}} = data;
+    const {id,firstName, lastName, profilePicture, contacts} = data;
 
-    const abbr = createElement('div', {classNames: ['initials']});
-    abbr.append(document.createTextNode(getAbbr(firstName, lastName)));
-    const picture = createImage({firstName, lastName, profilePicture, classNames: ['cardImage']});
-    const h3 = createElement('h3', {classNames: ['cardName'], textData: `${firstName} ${lastName}`});
+    let fullName = '';
+    if(firstName && lastName) {
+      fullName = `${firstName} ${lastName}`;
+    }
+
+    const abbr = createAbbr(data);    
+
+    const picture = createImage({classNames: ['cardImage'], attributes: {src: profilePicture, alt: fullName}, id: id});
+    const h3 = createElement('h3', {classNames: ['cardName'], textData: fullName});
     const p = createElement('p', {classNames: ['cardDescription'], textData: `Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo.`});
 
-    
-    const contacts = createElement('div', {classNames: ['contacts']});
+    const userContacts = createSocialLinks(contacts);
 
-    const cardImgWrap = createElement('div', {classNames: ['cardImageWrapper']},abbr, picture);
-
-    // const article = createElement('article', {classNames:['cardContainer']}, cardImgWrap, h3, p, contacts);
-    const article = createElement('article', {classNames:['cardContainer']}, cardImgWrap, h3, p);
+    const cardImgWrap = createElement('div', {classNames: ['cardImageWrapper'], attributes: {id: `wrapper${id}`}},abbr, picture);
+    cardImgWrap.style.backgroundColor = stringToColour(fullName);
+    const article = createElement('article', {classNames:['cardContainer']}, cardImgWrap, h3, p, userContacts);
 
     return createElement('li', {classNames: ["cardWrapper"]}, article);
   }
+
+  /*
+    CREATING ELEMENTS LOGIC
+  */
 
   /**
  *
@@ -36,39 +41,128 @@ new Map()
  * @param {object} options
  * @param {string[]} options.classNames - css classes
  * @param {function} options.onClick - click handler
+ * @param {Array} options.attributes - array of tuples with pair [attribute, value]
+ * @param {string} textData текстовая строка, находящаяся внутри элемента
  * @param {Node[]} children
  * @return {HTMLElement}
  */
-  function createElement(elementType, {classNames, onClick, attributes, textData} , ...children) {
+  function createElement(elementType, {classNames, onClick, attributes = {}, textData} , ...children) {
     const element = document.createElement(elementType);
+
     element.classList.add(...classNames);
+    for (const [attribute, value] of Object.entries(attributes)) {
+      element.setAttribute(attribute, value);
+    }
     if(textData) {
-      element.append(document.createTextNode(textData))
+      element.append(document.createTextNode(textData));
     };
     element.append(...children);
     return element;
   }
+  /**
+   * Функция создания элемента с инициалами
+   * @param {Object} name обьект с именем и фалилией
+   * @param {string} name.firstName
+   * @param {string} name.lastName
+   */
+  function createAbbr({firstName, lastName}) {
+    const abbr = createElement('div', {classNames: ['initials']});
+    abbr.append(document.createTextNode(getAbbr(firstName, lastName)));
+    return abbr;
+  }
 
-  // data и options одновременно не надо
   function createImage(options) {
-    const {firstName, lastName, profilePicture, classNames, } = options;
-    // const {classNames, onClick, src,} = options;
-    const img = createElement('img', {classNames});
-    img.src = profilePicture;
-    img.alt = `${firstName} ${lastName}`;
+    const { classNames , attributes , id} = options;
+    const img = createElement('img', {classNames, attributes});
+    img.dataset.id = id;
     img.addEventListener('error', handleImageError);
-    // img.addEventListener('load', handleImageLoad);
+    img.addEventListener('load', handleImageLoad);
     return img;
   }
 
-  function handleImageError({ target }) {
-    target.remove();
+  /**
+   * Функция обертка для создания группы социальных ссылок
+   * @param {Array} contacts массив контактов
+   * @return элемент div с ссылками на все полученные соц сети
+   */
+  function createSocialLinks (contacts) {
+    const linkHolder = [];
+    const userLinks = getSocialSites(contacts, faSites);
+
+    for (const link of userLinks) {
+      linkHolder.push(createSocialLink (link));
+    }
+    return  createElement('div', {classNames: ['contacts']}, ...linkHolder);
   }
 
-  function handleImageLoad({target,target: {dataset: { id },},}) {
-    document.getElementById(`wrapper${id}`).append(target);
+  /**
+   * Функция-обертка для создания ссылки в соц-сети
+   * @param {Map} Map 
+   * @param {} Map. 
+   */
+  function createSocialLink ([href, userClasses]) {
+    userClasses.unshift('fa', 'fa-fw', 'social-icon');
+    const linkImg = createElement('span', {classNames: userClasses});
+    return createElement('a', {classNames:['social-wrapper'], attributes : {href}}, linkImg);
   }
 
-  function getAbbr (firstName, lastName) {
-    return `${firstName.trim().charAt(0)} ${lastName.trim().charAt(0)}`;
+/*
+  UTILITY
+*/
+/**
+ * Функция получения инициалов
+ * @param {string} firstName 
+ * @param {string} lastName 
+ * @return {string} Инициалы или пустая строка
+ */
+function getAbbr (firstName, lastName) {
+  return firstName && lastName? `${firstName.trim().charAt(0)} ${lastName.trim().charAt(0)}` : '';
+}
+
+/**
+ * Функция создания связи между соц сетями и классами её гиперссылок 
+ * @param {Array} contacts массив с ссылками соц-сеть
+ * @param {Map} savedSocialSites ключ - домен соц-сети, значение - массив со строками классов
+ * @return {Map} ключ - гиперссылка на соц-сеть, значение - массив со строками классов
+ */
+function getSocialSites (contacts, savedSocialSites) {
+  const socialSites = new Map();
+  for(const link of contacts) {
+    const socialLink = new URL(link);
+    if(socialSites.get(socialLink)) {
+      continue;
+    }
+
+    for(const [hostname, faStyles] of  savedSocialSites) {
+      if(socialLink.hostname === hostname) {
+        socialSites.set(socialLink, faStyles);
+        break;
+      }
+    }
   }
+  return socialSites;
+}
+
+function stringToColour(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xff;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
+
+/*
+  HANDLERS
+*/
+function handleImageError({ target }) {
+  target.remove();
+}
+
+function handleImageLoad({target,target: {dataset: { id },},}) {
+  document.getElementById(`wrapper${id}`).append(target);
+}
