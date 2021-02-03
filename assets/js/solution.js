cd94e32262f26c199d05bbc7e2e2eda1df2b3a3c
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-02-03 20:45:17 +0200

    Add fetch

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,23 +1,21 @@
+'use strict';
 
-  const container = document.getElementById('root');
-  const cards = responseData.map((data) => createCard(data));
-  container.append(...cards);
 
-  function createCard(data) {
-    const {id,firstName, lastName, profilePicture, contacts} = data;
-    
-    const fullName = getFullName(firstName, lastName);
-    const abbr = createAbbr(fullName);    
-    
-    const nPicture = createElement('img',{eventListeners:{'error': handleImageError, 'load': handleImageLoad},classNames: ['card-image'], attributes: {'src': profilePicture, 'alt': fullName, 'data-id': id}});
-    const h3 = createElement('h3', {classNames: ['card-name'], textData: fullName});
-    const h4 = createElement('h4', {classNames: ['job-position'], textData: `Some important position, probably`});
-    const p = createElement('p', {classNames: ['card-description'], textData: `Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo.`});
-    const userContacts = createSocialLinks(contacts);
+const container = document.getElementById('root');
 
-    const cardImgWrap = createElement('div', {classNames: ['card-image-wrapper'], attributes: {id: `wrapper${id}`}},abbr, nPicture);
-    cardImgWrap.style.backgroundColor = stringToColour(fullName);
-    const article = createElement('article', {classNames:['card-container']}, cardImgWrap, h3, h4, p, userContacts);
+// fetch('http://192.168.1.148:3000/users')
+// .then((response)=> response.json()
+// .then((data)=>{
+//   const cards = data.map((card) => createCard(card));
+//   container.append(...cards); 
+// })).catch((err)=>{ console.log()});
 
-    return createElement('li', {eventListeners: {'mouseenter':changeColor, 'mouseleave': changeColor} ,classNames: ["card-wrapper"]}, article);
-  }
+fetch('./assets/js/data/data.json')
+.then((response)=> response.json()
+.then((celebrityList)=>{
+  const cards = celebrityList.map((celebrity) => createCard(celebrity));
+  container.append(...cards); 
+}));
+// const cards = responseData.map((data) => createCard(data));
+// container.append(...cards);
+
