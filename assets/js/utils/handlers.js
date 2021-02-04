9809e50f1b85c0030f7ada0604d4fbf370b59088
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-02-04 20:10:07 +0200

    Refactor selection logic

--- assets/js/utils/handlers.js
+++ assets/js/utils/handlers.js
@@ -6,15 +6,12 @@ function handleImageLoad({target,target: {dataset: { id },},}) {
   document.getElementById(`wrapper${id}`).append(target);
 }
 
-function toggleCelebritySelection() {
+function addCelebritySelection() {
   this.classList.toggle('card-selected');
 
   const celebrityFullName = this.children[0].children[1].textContent;
   const celebId = this.dataset.userId;
-  if(selectedCelebs.includes(celebId)) {
-      selectedCelebs.splice(selectedCelebs.indexOf(celebId), 1);
-      document.querySelector(`li[data-celeb-id="${celebId}"]`).remove();
-  } else {
+  if(!selectedCelebs.includes(celebId)) {
     selectedCelebs.push(celebId);
     selectedCelebsList.append(createElement('li',{classNames: ['celeb'],attributes:{'data-celeb-id':celebId}}, celebrityFullName));
   }
